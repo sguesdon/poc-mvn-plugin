7b49146477eccd07a6839b7f0706e794e15975e1
Sebastian Guesdon <sebastian.guesdon@gmail.com>
2021-09-27 19:43:20 +0200

    hello world goal

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,22 @@
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+@Mojo(name = "hello-world", defaultPhase = LifecyclePhase.COMPILE)
+public class HelloWorld extends AbstractMojo {
+
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    MavenProject project;
+
+    @Parameter(property = "scope")
+    String scope;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info("hello world :)");
+    }
+}
